b6b3127bc2564404f6fda1da3ca2597fbd92a053
Calvin Yau <calvinyau@berkeley.edu>
2016-07-14 06:04:53 -0700

    added redirect

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 var toggle = false;
+var count;
+var counter;
 chrome.tabs.executeScript(null, { file: "assets/jquery-3.0.0.min.js", allFrames: true});
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -13,29 +15,52 @@ chrome.runtime.onMessage.addListener(
                 // });
                 // sendResponse({}); // sending back empty response to sender
                 // break;
+                // chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
+                //     console.log("FIRST");
+                //     if (changeInfo.status == 'loading') {
+                //         console.log("LOADING");
+                //         chrome.tabs.getCurrent(function(tab){});
+                //         if (tab.url.includes("facebook.com") && changeInfo.url == undefined) {
+                //             console.log("HEllO");
+                //             alert("yo");
+                //         }
+                //         else if (changeInfo.url.includes("facebook.com")) {
+                //             // chrome.extension.sendRequest({redirect: "https://www.google.com"});
+                //             // this.close();
+                //             window.location.url = "https://www.google.com";
+                //             console.log("DSADASD");
+                //             alert("wtf");
+                //         }
+                //     }
+                // });
                 chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
+                    console.log("FIRST");
+                    // changeInfo.url is undefined when status == complete
                     if (changeInfo.status == 'loading') {
-                        if (changeInfo.url.includes("facebook.com")) {
-                            // chrome.extension.sendRequest({redirect: "https://www.google.com"});
-                            // window.location.replace("http://stackoverflow.com");
-                            // window.location = "http://google.com";
-                            // window.location.href = "http://google.com";
-                            // chrome.tabs.update(null, {url:"http://google.com"});
-                            alert("wtf");
-                            sendResponse({}); // sending back empty response to sender
-                            break;
+                        console.log("LOADING");
+                        //chrome.tabs.getCurrent(function(tab){});
+                        if (tab.url.includes("facebook.com") && changeInfo.url == undefined) {
+                            chrome.tabs.update({url: "https://google.com"});
+                            alert("Refresh");
                         }
-                        else if (tab.url.includes("facebook.com")) {
-                            alert("same");
+                        else if (changeInfo.url.includes("facebook.com")) {
+                            chrome.tabs.update({url: "https://google.com"});
+                            console.log(changeInfo.url);
+                            alert("New Page");
                         }
                     }
                 });
+                sendResponse({});
+                break;
             }
             else {
                 toggle = false;
                 sendResponse({});
                 break;
             }
+
+        case "time":
+            
 
         default:
             // helps debug when request directive doesn't match
