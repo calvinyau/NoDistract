b708810e3c6e9fdc748857eb1bcf053cf2041c97
Calvin Yau <calvinyau@berkeley.edu>
2016-08-27 05:27:22 -0700

    removed contentscript

--- contentscript.js
+++
@@ -1,5 +0,0 @@
-console.log("ASDASDASDASDAS");
-window.location.replace("http://stackoverflow.com");
-window.location = "http://google.com";
-window.location.href = "http://google.com";
-alert("wtf");
